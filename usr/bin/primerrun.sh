b2243cd0d61417e3c547852cf11fa946061a27df
alltern <143025426+alltern@users.noreply.github.com>
2025-11-19 18:09:52 -0500

    Update primerrun.sh

--- usr/bin/primerrun.sh
+++ usr/bin/primerrun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export GBM_BACKEND=nvidia-drm
+#export GBM_BACKEND=nvidia-drm
+#export VDPAU_DRIVER=nvidia
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export __NV_PRIME_RENDER_OFFLOAD=1
 export __VK_LAYER_NV_optimus=NVIDIA_only
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
-export VDPAU_DRIVER=nvidia
 export LIBVA_DRIVER_NAME=nvidia
 export NVD_BACKEND=direct
 echo "RUNNING $@"
