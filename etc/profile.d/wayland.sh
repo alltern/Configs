0fe6289c12936c79abf0001dc3c1489d56ed2904
alltern <143025426+alltern@users.noreply.github.com>
2025-11-22 23:20:35 -0500

    Update wayland.sh

--- etc/profile.d/wayland.sh
+++ etc/profile.d/wayland.sh
@@ -2,4 +2,6 @@ export ELECTRON_OZONE_PLATFORM_HINT=auto
 export EGL_PLATFORM=wayland
 export SDL_VIDEODRIVER=wayland
 export CLUTTER_BACKEND=wayland
+export GDK_BACKEND=wayland,x11,*
+export XDG_SESSION_TYPE=wayland
 #export PROTON_ENABLE_WAYLAND=1
