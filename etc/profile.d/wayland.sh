8cdcb1177916624e73694dc162102fb1aa08f848
alltern <143025426+alltern@users.noreply.github.com>
2025-11-22 23:37:06 -0500

    Update wayland.sh

--- etc/profile.d/wayland.sh
+++ etc/profile.d/wayland.sh
@@ -4,4 +4,5 @@ export SDL_VIDEODRIVER=wayland
 export CLUTTER_BACKEND=wayland
 export GDK_BACKEND=wayland,x11,*
 export XDG_SESSION_TYPE=wayland
+export QT_QPA_PLATFORM=wayland;xcb
 #export PROTON_ENABLE_WAYLAND=1
