e477406427471b174230f6332cb181cfccac8645
alltern <143025426+alltern@users.noreply.github.com>
2025-11-30 18:04:14 -0500

    Add files via upload

---
+++ BuildAndInstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Add wineprefix as an argument"
+  exit
+fi
+
+WINEPREFIX=$1
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $DIR
+
+rm -rf ./builds/
+./package-release.sh master ./builds/
+
+cd ./builds/dxvk-nvapi-master/
+
+cp ./x32/* $WINEPREFIX/drive_c/windows/syswow64/
+cp ./x64/* $WINEPREFIX/drive_c/windows/system32/
+
+cp ./layer/* ~/.local/share/vulkan/implicit_layer.d/
