27e72caff16be3bdfbf500cfad7ed866706dc78d
alltern <143025426+alltern@users.noreply.github.com>
2025-11-30 20:34:00 -0500

    Add files via upload

---
+++ XDGCHANGEDIRS.sh
@@ -0,0 +1,17 @@
+DIR=$1 
+
+APP=$2
+
+mkdir $DIR
+
+mkdir $DIR/cache
+mkdir $DIR/config
+mkdir $DIR/share
+
+export XDG_CONFIG_HOME=$DIR/config
+export XDG_DATA_HOME=$DIR/share
+export XDG_CACHE_HOME=$DIR/cache
+
+exec $APP
+
+
