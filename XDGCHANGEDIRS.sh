aa2096de69c6fb773367fe1bdd4d396f8ddd3dab
alltern <143025426+alltern@users.noreply.github.com>
2025-12-01 16:03:43 -0500

    Update XDGCHANGEDIRS.sh

--- XDGCHANGEDIRS.sh
+++ XDGCHANGEDIRS.sh
@@ -1,19 +1,34 @@
-#!/bin/bash
-
-DIR=$1 
+DIR=$1
 
 APP=$2
 
 mkdir $DIR
 
-mkdir $DIR/cache
-mkdir $DIR/config
-mkdir $DIR/share
+mkdir $DIR/.cache
+mkdir $DIR/.config
+mkdir $DIR/.local
+mkdir $DIR/.local/share
+mkdir $DIR/.local/state
+mkdir $DIR/.local/bin
 
-export XDG_CONFIG_HOME=$DIR/config
-export XDG_DATA_HOME=$DIR/share
-export XDG_CACHE_HOME=$DIR/cache
+mkdir $DIR/run
+mkdir $DIR/run/user
+mkdir $DIR/run/user/$UID
 
+mkdir $DIR/usr
+mkdir $DIR/usr/local
+mkdir $DIR/usr/local/share
+mkdir $DIR/usr/share
+
+mkdir $DIR/etc
+mkdir $DIR/etc/xdg
+
+export XDG_CONFIG_HOME=$DIR/.config
+export XDG_CACHE_HOME=$DIR/.cache
+export XDG_DATA_HOME=$DIR/.local/share
+export XDG_STATE_HOME=$DIR/.local/state
+export XDG_BIN_HOME=$DIR/.local/bin
+export XDG_RUNTIME_DIR=$DIR/run/user/$UID
+export XDG_DATA_DIRS='$DIR/usr/local/share:$DIR/usr/share'
+export XDG_CONFIG_DIRS=$DIR/etc/xdg
 exec $APP
-
-
