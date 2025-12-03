55eda2c745967c4d9e4a633485d8f5ae6d060d0b
alltern <143025426+alltern@users.noreply.github.com>
2025-12-03 00:23:20 -0500

    Update BuildAndInstall.sh

--- BuildAndInstall.sh
+++ BuildAndInstall.sh
@@ -2,12 +2,21 @@
 
 ### For use in DXVK-NVAPi: https://github.com/jp7677/dxvk-nvapi, put in the repo ###
 
+git pull
+git submodule init
+git submodule update --recursive
+
 if [ -z "$1" ]; then
   echo "Add wineprefix as an argument"
   exit
 fi
 
-WINEPREFIX=$1
+export WINEPREFIX=$1
+
+winetricks dotnet20 dotnet48 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_42 d3dx11_43 faudio vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 quartz
+
+setup_dxvk install
+setup_vkd3d_proton install
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $DIR
@@ -19,5 +28,18 @@ cd ./builds/dxvk-nvapi-master/
 
 cp ./x32/* $WINEPREFIX/drive_c/windows/syswow64/
 cp ./x64/* $WINEPREFIX/drive_c/windows/system32/
+
+echo 'Installing Nvidia Wine Driver Libraries'
+
+cp /lib64/nvidia/wine/* $WINEPREFIX/drive_c/windows/system32
+
+echo "Install layer to '~/.local/share/vulkan/implicit_layer.d/' [Y/n]"
+read -r YESNO
+if [ $YESNO = 'n' ] || [ $YESNO = 'N' ]; then
+  echo Not installing layer
+  exit
+fi
+
+echo "Installing layer to '~/.local/share/vulkan/implicit_layer.d/'"
 
 cp ./layer/* ~/.local/share/vulkan/implicit_layer.d/
