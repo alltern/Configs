6bbcebd0d5cda9b123d7719c6310aea00eed0b8f
alltern <143025426+alltern@users.noreply.github.com>
2025-12-03 00:26:47 -0500

    Update BuildAndInstall.sh

--- BuildAndInstall.sh
+++ BuildAndInstall.sh
@@ -13,10 +13,16 @@ fi
 
 export WINEPREFIX=$1
 
-winetricks dotnet20 dotnet48 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_42 d3dx11_43 faudio vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 quartz
 
-setup_dxvk install
-setup_vkd3d_proton install
+echo 'Initialize for the first time [y/N] '
+read -r INIT
+if [ $INIT = 'y' ] || [ $INIT = 'Y' ]; then
+  echo 'Initializing'
+  winetricks dotnet20 dotnet48 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_42 d3dx11_43 faudio vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 quartz
+
+  setup_dxvk install
+  setup_vkd3d_proton install
+fi
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $DIR
