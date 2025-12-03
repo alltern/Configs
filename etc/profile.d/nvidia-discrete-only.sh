5122538a594a07a3dd77c25e1915e2c38328c6d2
alltern <143025426+alltern@users.noreply.github.com>
2025-12-03 18:03:17 -0500

    Update nvidia-discrete-only.sh

--- etc/profile.d/nvidia-discrete-only.sh
+++ etc/profile.d/nvidia-discrete-only.sh
@@ -2,6 +2,7 @@
 #export VDPAU_DRIVER=nvidia
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+export VK_LOADER_DRIVERS_SELECT=*nvidia*
 export LIBVA_DRIVER_NAME=nvidia
 export NVD_BACKEND=direct
 
