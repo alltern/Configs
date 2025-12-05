97fe086f1f8740805b72c66dcb03ba918b0c595e
alltern <143025426+alltern@users.noreply.github.com>
2025-12-05 13:03:33 -0500

    Update nvidia-discrete-only.sh

--- etc/profile.d/nvidia-discrete-only.sh
+++ etc/profile.d/nvidia-discrete-only.sh
@@ -1,10 +1,13 @@
-#export GBM_BACKEND=nvidia-drm
-#export VDPAU_DRIVER=nvidia
+export GBM_BACKEND=nvidia-drm
+export WLR_NO_HARDWARE_CURSORS=1
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
 export VK_LOADER_DRIVERS_SELECT=*nvidia*
 export LIBVA_DRIVER_NAME=nvidia
 export NVD_BACKEND=direct
+
+#uncomment if you use VDPAU
+#export VDPAU_DRIVER=nvidia
 
 #UNCOMMENT BELOW if you use WINE with DXVK-NVAPI
 #export DXVK_ENABLE_NVAPI=1
